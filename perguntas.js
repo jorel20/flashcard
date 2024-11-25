6d7471cbeed8d9dde888df6e6517d7051a4093d0
jorel20 <106607135+jorel20@users.noreply.github.com>
2024-11-25 07:47:45 -0300

    251124

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Charadas',
+    'em que ano foi criada a banda colo de Deus?',
+    '2003'
+)
+
+criaCartao(
+    'Charadas',
+    'quantos anos a banda tem?',
+    '20'
+)
+
+criaCartao(
+    'Charadas',
+    'qual a sua musica mais tocada?',
+    'jesus meu esposo'
+)
